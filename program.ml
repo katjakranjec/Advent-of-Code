cb7c79a5044ac48e27fc8c75366804e5b4004732
Katja Kranjec <Kranjec.katja1@gmail.com>
2020-12-04 21:37:47 +0100

    Končana funkcija za day 4

--- program.ml
+++ program.ml
@@ -222,11 +222,149 @@ module Solver3 : Solver = struct
     rezultat
 end
 
+module Solver4 : Solver = struct
+
+  let rec en_passport seznam nov_passport = match seznam with
+          | [] -> []
+          | x :: xs -> if (String.length x) = 0 then nov_passport else en_passport xs (nov_passport @ [x])
+
+  let rec seznam_brez_prvih_n_elementov seznam n = if n <= 0 then seznam else seznam_brez_prvih_n_elementov (List.tl seznam) (n-1)
+
+  let rec seznam_passportov podatki nov_seznam = match podatki with
+    | [] -> nov_seznam
+    | _ -> seznam_passportov (seznam_brez_prvih_n_elementov (podatki) ((List.length (en_passport podatki [])) + 1)) (nov_seznam @ [ String.concat " " (en_passport podatki []) ])
+
+  let rec sezstr_to_sezsez sezstr nov_seznam = match sezstr with
+    | [] -> nov_seznam
+    | x :: xs -> sezstr_to_sezsez xs (nov_seznam @ [String.split_on_char ' ' x])
+
+  let seznam_locen_s_podpicjem seznam = String.split_on_char ':' (String.concat ":" seznam)
+
+  let seznam_vsebuje_cid seznam = List.mem "cid" seznam
+
+  let rec test sezsez stevilka = match sezsez with
+    | [] -> string_of_int stevilka
+    | x :: xs -> if ((List.length x) == 8) || (((List.length x) == 7) && (not (seznam_vsebuje_cid (seznam_locen_s_podpicjem x)))) then test xs (stevilka+1) else test xs stevilka
+
+  let naloga1 data =
+    let vrstice = String.split_on_char '\n' data in
+    let seznam_passport_stringov = seznam_passportov vrstice [] in
+    let seznam_passport_seznamov = sezstr_to_sezsez seznam_passport_stringov [] in
+    let rezultat = test seznam_passport_seznamov 0 in
+    rezultat
+
+  let rec sezstr_to_sezsez2 sezstr nov_seznam = match sezstr with
+  | [] -> nov_seznam
+  | x :: xs -> sezstr_to_sezsez2 xs (nov_seznam @ [String.split_on_char ':' x])
+
+  let rec sezsez_to_sezsezsez sezsez sezsezsez = match sezsez with
+    | [] -> sezsezsez
+    | x :: xs -> sezsez_to_sezsezsez xs (sezsezsez @ [(sezstr_to_sezsez2 x [])])
+
+  let test_je_digit x = match x with
+    | '0' -> true
+    | '1' -> true
+    | '2' -> true
+    | '3' -> true
+    | '4' -> true
+    | '5' -> true
+    | '6' -> true
+    | '7' -> true
+    | '8' -> true
+    | '9' -> true
+    | _ -> false
+
+  let test_je_letter x = match x with
+    | 'a' -> true
+    | 'b' -> true
+    | 'c' -> true
+    | 'd' -> true
+    | 'e' -> true
+    | 'f' -> true
+    | _ -> false
+
+  let test_je_c x = match x with
+    | 'c' -> true
+    | _ -> false
+
+  let test_je_m x = match x with
+    | 'm' -> true
+    | _ -> false
+
+  let test_je_i x = match x with
+    | 'i' -> true
+    | _ -> false
+
+  let test_je_n x = match x with
+    | 'n' -> true
+    | _ -> false
+
+  let zmanjkuje_mi_idej1 string =
+    let list = String.split_on_char 'c' string in
+    let zmanjkuje_mi_idej2 = if (150 <= (int_of_string (List.nth list 0))) && ((int_of_string (List.nth list 0)) <= 193) then true else false in
+    zmanjkuje_mi_idej2
+  
+  let zmanjkuje_mi_idej3 string =
+    let list = String.split_on_char 'i' string in
+    let zmanjkuje_mi_idej4 = if (59 <= (int_of_string (List.nth list 0))) && ((int_of_string (List.nth list 0)) <= 76) then true else false in
+    zmanjkuje_mi_idej4
+
+  let posebej_test_za_height y dolzina = match dolzina with
+    | 5 -> if (test_je_digit (y.[0])) && (test_je_digit (y.[2])) && (test_je_digit (y.[1])) && (test_je_c (y.[3])) && (test_je_m (y.[4])) then zmanjkuje_mi_idej1 y else false
+    | 4 -> if (test_je_digit (y.[0])) && (test_je_digit (y.[1])) && (test_je_i (y.[2])) && (test_je_n (y.[3])) then zmanjkuje_mi_idej3 y else false
+    | _ -> false
+
+  let testiranje_podatkov x y = match x with
+    | "byr" -> if ((String.length y) = 4) && (1920 <= (int_of_string y)) && ((int_of_string y) <= 2002) then true else false
+    | "iyr" -> if ((String.length y) = 4) && (2010 <= (int_of_string y)) && ((int_of_string y) <= 2020) then true else false
+    | "eyr" -> if ((String.length y) = 4) && (2020 <= (int_of_string y)) && ((int_of_string y) <= 2030) then true else false
+    | "hgt" -> posebej_test_za_height y (String.length y)
+    | "hcl" -> if (y.[0] = '#') && ((String.length y) = 7) && ((test_je_letter (y.[1])) || (test_je_digit (y.[1])))
+                   && ((test_je_letter (y.[2])) || (test_je_digit (y.[2]))) && ((test_je_letter (y.[3])) || (test_je_digit (y.[3])))
+                   && ((test_je_letter (y.[4])) || (test_je_digit (y.[4]))) && ((test_je_letter (y.[5])) || (test_je_digit (y.[5])))
+                   && ((test_je_letter (y.[6])) || (test_je_digit (y.[6])))
+                   then true else false
+    | "ecl" -> if (y = "amb") || (y = "blu") || (y = "brn") || (y = "gry") || (y = "grn") || (y = "hzl") || (y = "oth") then true else false
+    | "pid" -> if ((String.length y) = 9) && (test_je_digit (y.[0])) && (test_je_digit (y.[2])) && (test_je_digit (y.[3])) && (test_je_digit (y.[4]))
+                   && (test_je_digit (y.[5])) && (test_je_digit (y.[6])) && (test_je_digit (y.[7])) && (test_je_digit (y.[8])) && (test_je_digit (y.[1]))
+                   then true else false
+    | "cid" -> true
+    | _ -> false
+
+  let testiranje_validnosti par_v_obliki_seznama = match par_v_obliki_seznama with
+    | [] | _ :: [] -> failwith "Nekinedela"
+    | _ -> testiranje_podatkov (List.nth par_v_obliki_seznama 0) (List.nth par_v_obliki_seznama 1)
+
+  let rec test_ki_nardi_seznam sezsez nov_seznam = match sezsez with
+    | [] -> nov_seznam
+    | x :: xs -> if ((List.length x) == 8) || (((List.length x) == 7) && (not (seznam_vsebuje_cid (seznam_locen_s_podpicjem x)))) then test_ki_nardi_seznam xs (nov_seznam @ [x]) else test_ki_nardi_seznam xs nov_seznam
+
+  let rec se_en_test sezsezsez stevilka = match sezsezsez with
+    | [] -> string_of_int stevilka
+    | x1 :: rest1 -> let rec se_en_test2 sezsez = match sezsez with
+                    | [] -> se_en_test rest1 (stevilka + 1)
+                    | x2 :: rest2 -> if testiranje_validnosti x2 then se_en_test2 rest2 else se_en_test rest1 stevilka
+                    in 
+                    se_en_test2 x1
+
+  let naloga2 data _part1 =
+    let vrstice = String.split_on_char '\n' data in
+    let seznam_passport_stringov = seznam_passportov vrstice [] in
+    let seznam_passport_seznamov = sezstr_to_sezsez seznam_passport_stringov [] in
+    let tapravi = test_ki_nardi_seznam seznam_passport_seznamov [] in
+    let sezsezsez = sezsez_to_sezsezsez tapravi [] in
+    let rezultat = se_en_test sezsezsez 0 in
+    rezultat
+
+end
+
+
 (* Poženemo zadevo *)
 let choose_solver : string -> (module Solver) = function
   | "1" -> (module Solver1)
   | "2" -> (module Solver2)
   | "3" -> (module Solver3)
+  | "4" -> (module Solver4)
   | _ -> failwith "Ni še rešeno"
 
 let main () =
@@ -234,7 +372,6 @@ let main () =
   print_endline ("Solving DAY: " ^ day);
   let (module Solver) = choose_solver day in
   let input_data = preberi_datoteko ("data/day_" ^ day ^ ".in") in
-  print_endline input_data;
   let p1_start = Sys.time () in
   let part1 = Solver.naloga1 input_data in
   let t1_time = Sys.time () -. p1_start in
